cbbe6e24d5b072bb0c0605f24d2f64308330d628
V <you@example.com>
2024-04-03 17:40:35 -0600

    app ready

--- app.py
+++ app.py
@@ -1,84 +1,92 @@
-from flask import Flask, jsonify, request, abort
-from datetime import datetime
-from werkzeug.exceptions import BadRequest
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
 
-tasks = []
-BASE_URL = '/api/v1/'
+class Videojuego(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    desarrollador = db.Column(db.String(100), nullable=False)
+    anio_lanzamiento = db.Column(db.Integer, nullable=False)
+    plataforma = db.Column(db.String(100), nullable=False)
+    clasificacion = db.Column(db.String(50), nullable=False)
 
+    def __init__(self, titulo, desarrollador, anio_lanzamiento, plataforma, clasificacion):
+        self.titulo = titulo
+        self.desarrollador = desarrollador
+        self.anio_lanzamiento = anio_lanzamiento
+        self.plataforma = plataforma
+        self.clasificacion = clasificacion
 
-@app.route('/')
-def home():
-    return 'Welcome to my To-Do List'
-
-
-@app.route(BASE_URL + 'tasks', methods=['POST'])
-def create_task():
-    print("entreee")
-    if not request.json:
-        abort(404, error='Missing body in request')
-    name = request.json.get('name')
-    category = request.json.get('category')
-    name_check = True
-    category_check = True
-
-    if not name:
-        name_check = False
-    if not category:
-        category_check = False
-    if not name or not category:
-        return jsonify({'Name': name_check, 'Category': category_check}), 404
-
-
-    this_time = datetime.now()
-    task = {
-        'id':len(tasks) + 1,
-        'name' : request.json['name'],
-        'category' : request.json['category'],
-        'status' : False,
-        'created' : this_time,
-        'updated' : this_time
-    }
-    tasks.append(task)
-    return jsonify({'Task': task}), 201 
-
-
-@app.route(BASE_URL + 'tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({"tasks": tasks})
-
-@app.route(BASE_URL + 'tasks/<int:id>', methods=['GET'])
-def get_task(id):
-    this_task = [task for task in tasks if task['id'] == id]
-    print("TASK")
-    if len(this_task) == 0:
-        abort(404, error= 'ID not found')
-    return jsonify({'task': this_task[0]})
-
-######################
-@app.route("/tasks")
-
-@app.route(BASE_URL + 'tasks/<int:id>', methods=['PUT'])
-def check_task(id):
-    this_task = [task for task in tasks if task['id'] == id]
-    if len(this_task) == 0:
-        abort(404, error='ID not found')
-    this_task[0]['status'] = not this_task[0]['status']
-
-    return jsonify({'tasks': this_task[0]})
-
-@app.route(BASE_URL + 'tasks/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    this_task = [task for task in tasks if task['id'] == id]
-    if len(this_task) == 0:
-        abort(404, error='ID not found')
-    tasks.remove(this_task[0])
-    return jsonify({'result':True })
-
-    return jsonify({'tasks': this_task[0]})
-
-if __name__ == "__main__":
+    def to_json(self):
+        return {
+            'id': self.id,
+            'titulo': self.titulo,
+            'desarrollador': self.desarrollador,
+            'anio_lanzamiento': self.anio_lanzamiento,
+            'plataforma': self.plataforma,
+            'clasificacion': self.clasificacion
+        }
     
+    def __repr__(self):
+        return f'<Task {self.name}>'
+
+@app.route('/videojuegos', methods=['POST'])
+def add():
+    if not request.json or not 'titulo' in request.json:
+        abort(400)
+    videojuego = Videojuego(
+        titulo=request.json['titulo'],
+        desarrollador=request.json.get('desarrollador', ""),
+        anio_lanzamiento=request.json.get('anio_lanzamiento', 0),
+        plataforma=request.json.get('plataforma', ""),
+        clasificacion=request.json.get('clasificacion', "")
+    )
+    db.session.add(videojuego)
+    db.session.commit()
+    return jsonify(videojuego.to_json()), 201
+
+@app.route('/videojuegos', methods=['GET'])
+def get_all():
+    videojuegos = Videojuego.query.all()
+    return jsonify([videojuego.to_json() for videojuego in videojuegos])
+
+@app.route('/videojuegos/<int:id>', methods=['GET'])
+def get_one(id):
+    videojuego = Videojuego.query.get_or_404(id)
+    return jsonify(videojuego.to_json())
+
+@app.route('/videojuegos/<int:id>', methods=['PUT'])
+def update(id):
+    videojuego = Videojuego.query.get_or_404(id)
+    if not request.json:
+        abort(400)
+
+    videojuego.titulo = request.json.get('titulo', videojuego.titulo)
+    videojuego.desarrollador = request.json.get('desarrollador', videojuego.desarrollador)
+    videojuego.anio_lanzamiento = request.json.get('anio_lanzamiento', videojuego.anio_lanzamiento)
+    videojuego.plataforma = request.json.get('plataforma', videojuego.plataforma)
+    videojuego.clasificacion = request.json.get('clasificacion', videojuego.clasificacion)
+    
+    db.session.commit()
+    return jsonify(videojuego.to_json())
+
+@app.route('/videojuegos/<int:id>', methods=['DELETE'])
+def delete(id):
+    videojuego = Videojuego.query.get_or_404(id)
+    db.session.delete(videojuego)
+    db.session.commit()
+    return jsonify({'result': True})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    print("Task added!")
     app.run(debug=True)
